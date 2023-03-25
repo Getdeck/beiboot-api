92cf86a0a4ddaf9fa8d57b38e9bad8bf7897a468
Tobias Frölich <tobias@blueshoe.de>
2023-03-25 11:38:36 +0100

    wip: ts client cd

--- app/beiboot_rest/get_openapi.py
+++ app/beiboot_rest/get_openapi.py
@@ -2,11 +2,10 @@ from fastapi.openapi.utils import get_openapi
 from beiboot_rest import main
 import json
 
-with open("openapi.json", "w") as f:
-    json.dump(get_openapi(
-        title=main.app.title,
-        version=main.app.version,
-        openapi_version=main.app.openapi_version,
-        description=main.app.description,
-        routes=main.app.routes,
-        ), f)
+print(get_openapi(
+    title=main.app.title,
+    version=main.app.version,
+    openapi_version=main.app.openapi_version,
+    description=main.app.description,
+    routes=main.app.routes,
+    ))
