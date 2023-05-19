34f60bb2a4bf9c9feab856628a95e8431d0aaa4a
busch <christian@blueshoe.de>
2023-05-19 16:58:35 +0200

    test: fix list comparison

--- app/tests/test_group.py
+++ app/tests/test_group.py
@@ -6,33 +6,33 @@ class TestGroup:
         x_forwarded_groups = "beiboot-api-developer,beiboot-api-free"
         groups_filtered, group_selected = group_filter_and_selection(x_forwarded_groups=x_forwarded_groups)
 
-        assert groups_filtered == ["developer", "free"]
+        assert set(groups_filtered) == set(["developer", "free"])
         assert group_selected == "developer"
 
     def test_list_group_order(self):
         x_forwarded_groups = "beiboot-api-free"
         groups_filtered, group_selected = group_filter_and_selection(x_forwarded_groups=x_forwarded_groups)
 
-        assert groups_filtered == ["free"]
+        assert set(groups_filtered) == set(["free"])
         assert group_selected == "free"
 
     def test_list_empty(self):
         x_forwarded_groups = ""
         groups_filtered, group_selected = group_filter_and_selection(x_forwarded_groups=x_forwarded_groups)
 
-        assert groups_filtered == []
+        assert set(groups_filtered) == set([])
         assert group_selected == "default"
 
     def test_list_none(self):
         x_forwarded_groups = None
         groups_filtered, group_selected = group_filter_and_selection(x_forwarded_groups=x_forwarded_groups)
 
-        assert groups_filtered == []
+        assert set(groups_filtered) == set([])
         assert group_selected == "default"
 
     def test_list_invalid(self):
         x_forwarded_groups = "beiboot-api-invalid"
         groups_filtered, group_selected = group_filter_and_selection(x_forwarded_groups=x_forwarded_groups)
 
-        assert groups_filtered == []
+        assert set(groups_filtered) == set([])
         assert group_selected == "default"
